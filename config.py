7c9ea0571ae88dd5599775e70b14020f445803e6
MacBook <macbook@MacBooks-MacBook-Air.local>
2023-03-15 19:44:32 +0600

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "6113372900:AAEql10MKL5mieC9vyg4p1dOJ-RhAtNafc8"
+
+
+DATABASE_USER = "root"
+DATABASE_PASSWORD = "password"
+DATABASE_HOST = "localhost"
+DATABASE_NAME = "car_db"
+
+# Ссылка на базу данных
+MYSQL_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+
+
